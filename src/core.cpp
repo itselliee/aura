ba27b724b7f063cd13422c58e3cef79aa8d87601
Ellie (e35fmz) <ellie@horizon-net.work>
2026-08-03 20:29:24 +0100

    added a extra comment to make sure my environment works

--- src/core.cpp
+++ src/core.cpp
@@ -6,7 +6,10 @@ namespace AuraEngine {
 Core::Core() {}
 Core::~Core() {}
 
-void Core::Init() { std::cout << "Starting engine job.." << std::endl; }
+void Core::Init() {
+  std::cout << "Starting engine job.." << std::endl;
+  std::cout << "Aura-Engine specification 0.1, OpenGL 3.3" << std::endl;
+}
 
 void Core::Update() {}
 } // namespace AuraEngine
